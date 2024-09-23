b334752b095e6cc39b787b73e1da52d0c5e642cb
Maplefallin <2107522784@qq.com>
2024-09-23 09:05:43 +0800

    cqx

--- test1/flower_classification.py
+++ test1/flower_classification.py
@@ -1,10 +1,11 @@
 #利用鸢尾花数据集，实现前向传播、反向传播、可视化loss曲线
 
 import tensorflow as tf
+from numpy.ma.core import argmax
 from  sklearn import datasets
 from matplotlib import pyplot as plt
 import numpy as np
-
+from tensorboard.compat.tensorflow_stub.dtypes import int32
 
 #导入数据集并划分特征和标签
 x_data = datasets.load_iris().data
@@ -25,10 +26,10 @@ y_test = y_data[-30:]
 
 #将测试机训练中的标签与特征一一对应,并且分批次
 x_train = tf.cast(x_train,tf.float32)
-x_test = tf.cast(x_train,tf.float32)
+x_test = tf.cast(x_test,tf.float32)
 
 train_db = tf.data.Dataset.from_tensor_slices((x_train,y_train)).batch(32)
-# test_db = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
+test_db = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
 
 w1 = tf.Variable(tf.random.truncated_normal([4,3],stddev=0.1,seed=1))
 b1 = tf.Variable(tf.random.truncated_normal([3],stddev=0.1,seed=1))
@@ -46,7 +47,7 @@ for epoch in range(epoch):
             y = tf.matmul(x_train,w1)+b1
             y = tf.nn.softmax(y)
             y_ = tf.one_hot(y_train,depth=3)
-            loss = tf.reduce_mean(tf.square(y_-y))
+            loss = tf.reduce_mean(tf.square(y_-y))#均方误差损失函数mse
             loss_all += loss.numpy()
         grads = tape.gradient(loss,[w1,b1])
 
@@ -57,3 +58,33 @@ for epoch in range(epoch):
     train_loss_results.append(loss_all / 4)  # 将4个step的loss求平均记录在此变量中
     loss_all = 0  # loss_all归零，为记录下一个epoch的loss做准备
 
+    total_correct, total_number = 0, 0
+    for x_test, y_test in test_db:
+        y = tf.matmul(x_test,w1) + b1
+        y = tf.nn.softmax(y)
+        pred = tf.argmax(y,axis=1)
+        pred = tf.cast(pred,dtype=y_test.dtype)
+        correct = tf.cast(tf.equal(pred,y_test),dtype=int32)
+        correct = tf.reduce_sum(correct)
+        total_correct += int(correct)
+        total_number += x_test.shape[0]
+    acc = total_correct/total_number
+    test_acc.append(acc)
+    print("Test_acc:", acc)
+    print("--------------------------")
+
+# 绘制 loss 曲线
+plt.title('Loss Function Curve')  # 图片标题
+plt.xlabel('Epoch')  # x轴变量名称
+plt.ylabel('Loss')  # y轴变量名称
+plt.plot(train_loss_results, label="$Loss$")  # 逐点画出trian_loss_results值并连线，连线图标是Loss
+plt.legend()  # 画出曲线图标
+plt.show()  # 画出图像
+
+# 绘制 Accuracy 曲线
+plt.title('Acc Curve')  # 图片标题
+plt.xlabel('Epoch')  # x轴变量名称
+plt.ylabel('Acc')  # y轴变量名称
+plt.plot(test_acc, label="$Accuracy$")  # 逐点画出test_acc值并连线，连线图标是Accuracy
+plt.legend()
+plt.show()
